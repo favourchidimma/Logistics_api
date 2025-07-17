f2ba228e24fb2c9f2bf1519263e81af9b8c1c235
favourchidimma <idikafavourchidimma1@gmail.com>
2025-07-17 15:35:45 +0200

    deploy changes

--- Logistics/Logistics/settings.py
+++ Logistics/Logistics/settings.py
@@ -129,7 +129,7 @@ STATIC_URL = 'static/'
 
 STATIC_FILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-# Default primary key field type
+# Default primary key field typ
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
